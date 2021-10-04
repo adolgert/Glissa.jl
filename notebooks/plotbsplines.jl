227d451d581acf61e8fd1b55ab056489ed48e008
Drew Dolgert <adolgert@uw.edu>
2021-10-04 09:01:35 -0700

    works for internal knots

--- notebooks/plotbsplines.jl
+++ notebooks/plotbsplines.jl
@@ -20,24 +20,6 @@ function evaluate(totalaxis::AbstractVector, bsplines::AbstractVector, x)
   total
 end
 
-order = 3
-T = Float64
-axis = T[0, 1, 2, 3, 4]
-multiplicity = [order, 2, 2, 2, order]
-@assert length(multiplicity) == length(axis)
-vcnt = sum(multiplicity)
-a = 0
-b = 5
-
-splines = Vector{PolyBSpline{T,T}}(undef, sum(multiplicity) - order)
-for i in 1:(sum(multiplicity) - order)
-	((l, r), m) = Glissa.reduce_axis(multiplicity, order, i)
-	coeffs = zeros(T, order, r - l + 1)
-	println("$m")
-	Glissa.bspline_by_matrix!(axis[l:r], coeffs, m, order, :N)
-	splines[i] = PolyBSpline{T,T}(axis[l:r], coeffs, UnitRange{Int}(l, r))
-end
-
 function plotsplines(splines, i, append = false)
 	xp = splines[i].τ[1]:0.1:splines[i].τ[end]
 	yp = splines[i].(xp)
@@ -57,6 +39,25 @@ function plotallsplines(splines)
 		plotsplines(splines, i, true)
 	end
 end
+
+order = 3
+T = Float64
+axis = T[0, 1, 2, 3, 4]
+multiplicity = [order, 2, 2, 2, order]
+@assert length(multiplicity) == length(axis)
+vcnt = sum(multiplicity)
+a = 0
+b = 5
+
+splines = Vector{PolyBSpline{T,T}}(undef, sum(multiplicity) - order)
+for i in 1:(sum(multiplicity) - order)
+	((l, r), m) = Glissa.reduce_axis(multiplicity, order, i)
+	coeffs = zeros(T, order, r - l + 1)
+	println("$m")
+	Glissa.bspline_by_matrix!(axis[l:r], coeffs, m, order, :Q)
+	splines[i] = PolyBSpline{T,T}(axis[l:r], coeffs, UnitRange{Int}(l, r))
+end
+
 
 length(splines)
 plotsplines(splines, 3, true)
